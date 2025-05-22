3dd7590154f10b34673557b74b738cd78fc966d2
Mariyem Ahmed <mariyemahmed@Mariyems-MacBook-Pro.local>
2025-05-22 18:37:31 +0500

    Day 2

--- task1.py
+++ task1.py
@@ -18,7 +18,48 @@ category = set()
 # List of actions done
 actions = []
 
+# Decorator for adding product
+def add_decorator(f):
+    def wrap(inventory, name, price, quantity):
+        print("Actions before adding product: ", actions)
+        f(inventory, name, price, quantity)
+        print("Actions after adding product: ", actions)
+    return wrap
+
+# Decorator for updating stock
+def update_decorator(f):
+    def wrap(inventory, name, change):
+        print("Quantity before updating stock: ", inventory[name].quantity)
+        f(inventory, name, change)
+        print("Quantity after updating stock: ", inventory[name].quantity)
+    return wrap
+
+# Decorator for displaying inventory
+def display_decorator(f):
+    def wrap(inventory):
+        print("\nBefore displaying inventory")
+        f(inventory)
+        print("After displaying inventory")
+    return wrap
+
+# Decorator for removing product
+def remove_decorator(f):
+    def wrap(inventory, name):
+        print("\nBefore removing product: ", actions)
+        f(inventory, name)
+        print("After removing product: ", actions)
+    return wrap
+
+# Decorator for searching
+def search_decorator(f):
+    def wrap(inventory, name):
+        print("\nBefore searching product")
+        f(inventory, name)
+        print("After searching product")
+    return wrap
+
 # add_product(inventory, name, price, quantity) → Adds a new product.
+@add_decorator
 def add_product(inventory, name, price, quantity):
     inventory[name] = Product(name, price, quantity)
 
@@ -29,32 +70,36 @@ def add_product(inventory, name, price, quantity):
     actions.append(f'Added {name}')
 
 # update_quantity(inventory, name, change) → Adjusts stock (+/-).
+@update_decorator
 def update_quantity(inventory, name, change):
-    if name in inventory:
+    try:
         inventory[name].quantity += change
         actions.append("Updated Stock")
-    else:
+    except KeyError:
         print("Name doesn't exist")
 
 
 # remove_product(inventory, name) → Deletes a product.
+@remove_decorator
 def remove_product(inventory, name):
-    if name in inventory:
+    try:
         del inventory[name]
         actions.append("Removed " + name)
-    else:
+    except KeyError:
         print("Name doesn't exist")
 
 # display_inventory(inventory) → Prints all products.
+@display_decorator
 def display_inventory(inventory):
     for item in inventory:
         print(f'Name: {inventory[item].name}, Price: {inventory[item].price}, Quantity: {inventory[item].quantity}')
 
 # search_product(inventory, name) → Shows product details if found.
+@search_decorator
 def search_product(inventory, name):
-    if name in inventory:
-        print(f'Name: {name}, Price: {inventory[name].price}, Quantity: {inventory[name].quantity}')
-    else:
+    try:
+        print(f'Item found:\nName: {name}, Price: {inventory[name].price}, Quantity: {inventory[name].quantity}')
+    except KeyError:
         print("Name not found")
 
 
@@ -74,7 +119,10 @@ while not quit:
     elif entry == 2:
         name = input("Name: ")
         change = int(input("Add quantity: "))
-        update_quantity(inventory, name, change)
+        try:
+            update_quantity(inventory, name, change)
+        except KeyError:
+            print("Name doesn't exist")
     elif entry == 3:
         name = input("Name: ")
         remove_product(inventory, name)
@@ -94,5 +142,5 @@ while not quit:
         continue
 
     # Debug
-    print ("\tCategories: ", category)
-    print ("\tActions done: ", actions)
+    print ("\nDebugging\n\tCategories: ", category)
+    print ("\tActions done: ", actions, "\n")
